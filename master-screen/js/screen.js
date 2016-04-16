f74cd180bee7d6aadb13d5c5eae8018388dd7fb7
Babibubebon <babibubebon@babibubebo.org>
2016-04-16 17:47:42 +0900

    [Screen] フレーム機能を追加

--- master-screen/js/screen.js
+++ master-screen/js/screen.js
@@ -1,15 +1,16 @@
-var ParaparaScreen = function (screen, parent) {
+var ParaparaScreen = function (screen) {
   this.frames = [];
-  this.parent = parent;
+  this.currentIndex = 0;
   
   if (screen) {
     this.canvas = screen;
     this.context = screen.getContext("2d");
-  } else if (parent) {
+  } else {
     this.canvas = document.createElement("canvas");
     this.context = this.canvas.getContext("2d");
-    parent.appendChild(this.canvas);
   }
+  
+  this.addFrame();
 };
 
 ParaparaScreen.prototype.setSize = function (width, height) {
@@ -20,6 +21,49 @@ ParaparaScreen.prototype.setSize = function (width, height) {
 ParaparaScreen.prototype.setPosition = function (x, y) {
   this.canvas.style.left = x + "px";
   this.canvas.style.top = y + "px";
+};
+
+ParaparaScreen.prototype.setId = function (id) {
+  this.canvas.id = id;
+};
+
+ParaparaScreen.prototype.getImageData = function () {
+  return this.context.getImageData(0, 0, this.canvas.width, this.canvas.height);
+};
+
+ParaparaScreen.prototype.addFrame = function (index) {
+  if (index === undefined || this.frames.length <= index) {
+    index = this.frames.length;
+  }
+  
+  var img = this.context.createImageData(this.canvas.width, this.canvas.height);
+  this.frames.splice(index, 0, img);
+};
+
+ParaparaScreen.prototype.removeFrame = function (index) {
+  if (0 <= index && index < this.frames.length) {
+    this.frames.splice(index, 1);
+  }
+  if (this.frames.length > 0 && this.currentIndex === index) {
+    // 現在のフレームを削除する場合
+    if (this.frames.length === index) {
+      // 末尾
+      index = index - 1;
+    }
+    this.context.putImageData(this.frames[index], 0, 0);
+  }
+};
+
+ParaparaScreen.prototype.changeCurrentFrame = function (index) {
+  if (index < this.frames.length) {
+    var currentImage = this.getImageData();
+    this.frames[this.currentIndex] = currentImage;
+    
+    var img = this.frames[index];
+    this.context.putImageData(img, 0, 0);
+    
+    this.currentIndex = index;
+  }
 };
 
 ParaparaScreen.prototype.draw = function (begin, end, options) {
@@ -43,20 +87,27 @@ var ScreenServer = function(elm) {
 
 ScreenServer.prototype.appendScreen = function (id) {
   if ( ! (id in this.screens)) {
-    this.screens[id] = new ParaparaScreen(null, this.element);
+    this.screens[id] = new ParaparaScreen(null);
+    this.screens[id].setId(id);
+    this.element.appendChild(this.screens[id].canvas);
+    
+    return this.screens[id];
   }
+  return false;
 };
 
 ScreenServer.prototype.removeScreen = function (id) {
   if (id in this.screens) {
-    delete this.addScreen[id];
+    this.element.removeChild(this.screens[id].canvas);
+    delete this.screens[id];
   }
 };
 
+ScreenServer.prototype.getScreens = function () {
+  return this.screens;
+};
+
 ScreenServer.prototype.getScreen = function (id) {
-  if (id === undefined) {
-    return this.screens;
-  }
   if (id in this.screens) {
     return this.screens[id];
   }
